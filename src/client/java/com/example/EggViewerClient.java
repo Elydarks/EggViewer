8b2d50832119f8042c4f57b57730dc40796317ef
Elydarks <btbgames12@gmail.com>
2024-07-04 20:49:36 +0200

    Added some functions

--- src/client/java/com/example/EggViewerClient.java
+++ src/client/java/com/example/EggViewerClient.java
@@ -2,11 +2,16 @@ package com.example;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtList;
+import net.minecraft.text.MutableText;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.text.TextColor;
@@ -17,159 +22,201 @@ import org.slf4j.LoggerFactory;
 import java.util.List;
 
 public class EggViewerClient implements ClientModInitializer {
-	public static final Logger LOGGER = LoggerFactory.getLogger("eggviewer");
+    public static final Logger LOGGER = LoggerFactory.getLogger("eggviewer");
 
-	@Override
-	public void onInitializeClient() {
-		// Register the tooltip event
-		ItemTooltipCallback.EVENT.register(this::onItemTooltip);
+    @Override
+    public void onInitializeClient() {
+        // Register the tooltip event
+        ItemTooltipCallback.EVENT.register(this::onItemTooltip);
 
-	}
+        HudRenderCallback.EVENT.register(this::onRenderHud);
 
-	private void onItemTooltip(ItemStack stack, TooltipContext context, List<Text> lines) {
-		if (stack.getItem() == Items.TURTLE_EGG) {
-			NbtCompound nbt = stack.getNbt();
-			if (nbt != null) {
-				NbtCompound polymer = nbt.getCompound("Polymer$itemTag");
-				NbtCompound ivs = polymer.getCompound("IVs");
-				NbtList eggMoves = polymer.getList("BenchedMoves", 10);
-				String ability = polymer.getCompound("Ability").getString("AbilityName");
+    }
 
-				String gender = polymer.getString("Gender");
+    private void onRenderHud(DrawContext drawContext, float tickDelta) {
+        MinecraftClient client = MinecraftClient.getInstance();
+        PlayerEntity player = client.player;
+        if (player == null) {
+            return;
+        }
 
-				String genderSymbol;
-				Style genderColor;
+        ItemStack stack = player.getMainHandStack();
+        if (stack.getItem() == Items.TURTLE_EGG) {
+            NbtCompound nbt = stack.getNbt();
+            if (nbt != null) {
+                NbtCompound polymer = nbt.getCompound("Polymer$itemTag");
+                int cyclesLeft = polymer.getInt("currentEggCycle");
+                double stepsLeft = polymer.getDouble("stepsLeftInCycle");
 
-				if (gender.equals("FEMALE")) {
-					genderSymbol = "♀";
-					genderColor = Style.EMPTY.withColor(Formatting.LIGHT_PURPLE);
-				} else {
-					genderSymbol = "♂";
-					genderColor = Style.EMPTY.withColor(Formatting.AQUA);
-				}
+                int green = (int) (255 * (cyclesLeft / 30.0));
+                int red = 255 - green;
+                int color = (red << 16) | (green << 8);
 
-				String shinySymbol = "";
-				if (polymer.getBoolean("Shiny")) {
-					shinySymbol = " ★";
-				}
-				String Form = polymer.getString("FormId") + " ";
+                MutableText steps = Text.literal("Cycles: " +cyclesLeft)
+                        .setStyle(Style.EMPTY.withColor(Formatting.GREEN)).setStyle(Style.EMPTY.withBold(true))
+                        .append(Text.literal(" Steps: " + (int) stepsLeft).setStyle(Style.EMPTY.withBold(false))
+                                .setStyle(Style.EMPTY.withColor(TextColor.fromRgb(color))));
 
-				String AbilityPrio = polymer.getCompound("Ability").getString("AbilityPriority");
-				String HA = "No";
-				if (!AbilityPrio.equals("LOWEST")) {
-					HA = "yes";
-				}
+                int screenWidth = client.getWindow().getScaledWidth();
+                int screenHeight = client.getWindow().getScaledHeight();
+                int textWidth = client.textRenderer.getWidth(steps);
+                int x = (screenWidth / 2) - (textWidth / 2);
+                int y = screenHeight - 70;
 
-				lines.add(Text.literal("Form: ").setStyle(Style.EMPTY.withColor(Formatting.DARK_PURPLE))
-						.append(Text.literal(Form)).append(Text.literal(genderSymbol).setStyle(genderColor)
-								.append(Text.literal(shinySymbol).setStyle(Style.EMPTY.withColor(Formatting.GOLD)))));
+                drawContext.drawText(client.textRenderer, steps, x, y, 0xFFFFFF, true);
+            }
+        }
+    }
 
-				lines.add(Text.literal("Ability: ").setStyle(Style.EMPTY.withColor(Formatting.GOLD))
-						.append(Text.literal(String.valueOf(ability)).setStyle(Style.EMPTY.withColor(Formatting.WHITE)))
-						.append(Text.literal(" | ").setStyle(Style.EMPTY.withColor(Formatting.WHITE)))
-						.append(Text.literal("HA: ").setStyle(Style.EMPTY.withColor(Formatting.GOLD)))
-						.append(Text.literal(HA).setStyle(Style.EMPTY.withColor(Formatting.WHITE))));
+    private void onItemTooltip(ItemStack stack, TooltipContext context, List<Text> lines) {
+        if (stack.getItem() == Items.TURTLE_EGG) {
+            NbtCompound nbt = stack.getNbt();
+            if (nbt != null) {
+                NbtCompound polymer = nbt.getCompound("Polymer$itemTag");
+                if (polymer != null) {
+                    NbtCompound ivs = polymer.getCompound("IVs");
+                    NbtList eggMoves = polymer.getList("BenchedMoves", 10);
+                    String ability = polymer.getCompound("Ability").getString("AbilityName");
+
+                    String gender = polymer.getString("Gender");
+
+                    String genderSymbol;
+                    Style genderColor;
+
+                    if (gender.equals("FEMALE")) {
+                        genderSymbol = "♀";
+                        genderColor = Style.EMPTY.withColor(Formatting.LIGHT_PURPLE);
+                    } else {
+                        genderSymbol = "♂";
+                        genderColor = Style.EMPTY.withColor(Formatting.AQUA);
+                    }
+
+                    String shinySymbol = "";
+                    if (polymer.getBoolean("Shiny")) {
+                        shinySymbol = " ★";
+                    }
+                    String Form = polymer.getString("FormId") + " ";
+
+                    String AbilityPrio = polymer.getCompound("Ability").getString("AbilityPriority");
+                    String HA = "No";
+                    if (!AbilityPrio.equals("LOWEST")) {
+                        HA = "yes";
+                    }
+
+                    lines.add(Text.literal("Form: ").append(Text.literal(Form)).setStyle(Style.EMPTY.withColor(Formatting.DARK_PURPLE))
+                            .append(Text.literal(genderSymbol).setStyle(genderColor)
+                                    .append(Text.literal(shinySymbol).setStyle(Style.EMPTY.withColor(Formatting.GOLD)))));
+
+                    lines.add(Text.literal("Ability: ").setStyle(Style.EMPTY.withColor(Formatting.GOLD))
+                            .append(Text.literal(String.valueOf(ability)).setStyle(Style.EMPTY.withColor(Formatting.WHITE)))
+                            .append(Text.literal(" | ").setStyle(Style.EMPTY.withColor(Formatting.WHITE)))
+                            .append(Text.literal("HA: ").setStyle(Style.EMPTY.withColor(Formatting.GOLD)))
+                            .append(Text.literal(HA).setStyle(Style.EMPTY.withColor(Formatting.WHITE))));
+
+                    int HPtype = this.CalcHPType(ivs);
+                    int HPDamage = this.CalcHPDamage(ivs);
+
+                    lines.add(Text.literal("Hidden power: ").setStyle(Style.EMPTY.withColor(Formatting.DARK_GREEN))
+                            .append(Text.literal(HPDamage + " ").setStyle(Style.EMPTY.withColor(Formatting.WHITE)))
+                            .append(Text.literal(Type.values()[HPtype].name()).setStyle(Style.EMPTY.withColor(Type.values()[HPtype].getColor()))));
+
+                    lines.add(Text.literal("\n"));
+
+                    lines.add(Text.literal("HP: ").setStyle(Style.EMPTY.withColor(Formatting.RED))
+                            .append(Text.literal(String.valueOf(ivs.getInt("hp")))
+                                    .setStyle(Style.EMPTY.withColor(Formatting.WHITE))));
+
+                    lines.add(Text.literal("Attack: ").setStyle(Style.EMPTY.withColor(Formatting.BLUE))
+                            .append(Text.literal(String.valueOf(ivs.getInt("attack")))
+                                    .setStyle(Style.EMPTY.withColor(Formatting.WHITE))));
+
+                    lines.add(Text.literal("Defense: ").setStyle(Style.EMPTY.withColor(Formatting.GRAY))
+                            .append(Text.literal(String.valueOf(ivs.getInt("defence")))
+                                    .setStyle(Style.EMPTY.withColor(Formatting.WHITE))));
+
+                    lines.add(Text.literal("Special Attack: ").setStyle(Style.EMPTY.withColor(Formatting.AQUA))
+                            .append(Text.literal(String.valueOf(ivs.getInt("special_attack")))
+                                    .setStyle(Style.EMPTY.withColor(Formatting.WHITE))));
+
+                    lines.add(Text.literal("Special Defence: ").setStyle(Style.EMPTY.withColor(Formatting.YELLOW))
+                            .append(Text.literal(String.valueOf(ivs.getInt("special_defence")))
+                                    .setStyle(Style.EMPTY.withColor(Formatting.WHITE))));
+
+                    lines.add(Text.literal("Speed: ").setStyle(Style.EMPTY.withColor(Formatting.GREEN))
+                            .append(Text.literal(String.valueOf(ivs.getInt("speed")))
+                                    .setStyle(Style.EMPTY.withColor(Formatting.WHITE))));
 
 
-				lines.add(Text.literal("HP: ").setStyle(Style.EMPTY.withColor(Formatting.RED))
-						.append(Text.literal(String.valueOf(ivs.getInt("hp")))
-								.setStyle(Style.EMPTY.withColor(Formatting.WHITE))));
+                    if (!eggMoves.isEmpty()) {
+                        lines.add(Text.literal("\n"));
 
-				lines.add(Text.literal("Attack: ").setStyle(Style.EMPTY.withColor(Formatting.BLUE))
-						.append(Text.literal(String.valueOf(ivs.getInt("attack")))
-								.setStyle(Style.EMPTY.withColor(Formatting.WHITE))));
+                        lines.add(Text.literal("Egg moves: ").setStyle(Style.EMPTY.withColor(Formatting.DARK_PURPLE)));
 
-				lines.add(Text.literal("Defense: ").setStyle(Style.EMPTY.withColor(Formatting.GRAY))
-						.append(Text.literal(String.valueOf(ivs.getInt("defence")))
-								.setStyle(Style.EMPTY.withColor(Formatting.WHITE))));
+                        for (int i = 0; i < eggMoves.size(); i++) {
+                            NbtCompound moveTag = eggMoves.getCompound(i);
+                            lines.add(Text.literal("  " + moveTag.getString("MoveName")).setStyle(Style.EMPTY.withColor(Formatting.WHITE)));
+                            if (i < eggMoves.size() - 1) {
+                                lines.add(Text.literal(", ").setStyle(Style.EMPTY.withColor(Formatting.WHITE)));
+                            }
+                        }
+                    }
 
-				lines.add(Text.literal("Special Attack: ").setStyle(Style.EMPTY.withColor(Formatting.AQUA))
-						.append(Text.literal(String.valueOf(ivs.getInt("special_attack")))
-								.setStyle(Style.EMPTY.withColor(Formatting.WHITE))));
+                }
+            }
+        }
+    }
 
-				lines.add(Text.literal("Special Defence: ").setStyle(Style.EMPTY.withColor(Formatting.YELLOW))
-						.append(Text.literal(String.valueOf(ivs.getInt("special_defence")))
-								.setStyle(Style.EMPTY.withColor(Formatting.WHITE))));
+    private int CalcHPType(NbtCompound ivs) {
+        int a = ivs.getInt("hp") % 2;
+        int b = ivs.getInt("attack") % 2;
+        int c = ivs.getInt("defence") % 2;
+        int d = ivs.getInt("speed") % 2;
+        int e = ivs.getInt("special_attack") % 2;
+        int f = ivs.getInt("special_defence") % 2;
 
-				lines.add(Text.literal("Speed: ").setStyle(Style.EMPTY.withColor(Formatting.GREEN))
-						.append(Text.literal(String.valueOf(ivs.getInt("speed")))
-								.setStyle(Style.EMPTY.withColor(Formatting.WHITE))));
+        int typeBits = a + 2 * b + 4 * c + 8 * d + 16 * e + 32 * f;
+        return (int) Math.floor((typeBits * 15) / 63.0);
+    }
 
-				int HPtype = this.CalcHPType(ivs);
-				int HPDamage = this.CalcHPDamage(ivs);
+    private int CalcHPDamage(NbtCompound ivs) {
+        int u = (ivs.getInt("hp") % 4) / 2;
+        int v = (ivs.getInt("attack") % 4) / 2;
+        int w = (ivs.getInt("defence") % 4) / 2;
+        int x = (ivs.getInt("speed") % 4) / 2;
+        int y = (ivs.getInt("special_attack") % 4) / 2;
+        int z = (ivs.getInt("special_defence") % 4) / 2;
 
-				lines.add(Text.literal("Hidden power: ").setStyle(Style.EMPTY.withColor(Formatting.DARK_GREEN))
-						.append(Text.literal(HPDamage + " ").setStyle(Style.EMPTY.withColor(Formatting.WHITE)))
-						.append(Text.literal(Type.values()[HPtype].name()).setStyle(Style.EMPTY.withColor(Type.values()[HPtype].getColor()))));
+        int powerBits = u + 2 * v + 4 * w + 8 * x + 16 * y + 32 * z;
 
-				if (!eggMoves.isEmpty()) {
-					String eggmovesString = "[ \n";
-					for (int i= 0; i < eggMoves.size(); i++) {
-						NbtCompound moveTag = eggMoves.getCompound(i);
-						eggmovesString += "  " + moveTag.getString("MoveName");
-						if (i < eggMoves.size() - 1) {
-							eggmovesString += ", \n";
-						}
-					}
-					eggmovesString += "\n ]";
-					lines.add(Text.literal("eggmoves: ").setStyle(Style.EMPTY.withColor(Formatting.DARK_PURPLE)).append(Text.literal(eggmovesString).setStyle(Style.EMPTY.withColor(Formatting.WHITE))));
-				}
+        return (int) Math.floor(((powerBits * 40) / 63.0) + 30);
+    }
 
-			}
-		}
-	}
+    private enum Type {
+        FIGHTING(0xC22E28),
+        FLYING(0xA98FF3),
+        POISON(0xA33EA1),
+        GROUND(0xE2BF65),
+        ROCK(0xB6A136),
+        BUG(0xA6B91A),
+        GHOST(0x735797),
+        STEEL(0xB7B7CE),
+        FIRE(0xEE8130),
+        WATER(0x6390F0),
+        GRASS(0x7AC74C),
+        ELECTRIC(0xF7D02C),
+        PSYCHIC(0xF95587),
+        ICE(0x96D9D6),
+        DRAGON(0x6F35FC),
+        DARK(0x705746);
 
-	private int CalcHPType(NbtCompound ivs) {
-		int a = ivs.getInt("hp") % 2;
-		int b = ivs.getInt("attack") % 2;
-		int c = ivs.getInt("defence") % 2;
-		int d = ivs.getInt("speed") % 2;
-		int e = ivs.getInt("special_attack") % 2;
-		int f = ivs.getInt("special_defence") % 2;
+        private final TextColor color;
 
-		int typeBits = a + 2 * b + 4 * c + 8 * d + 16 * e + 32 * f;
-		return (int) Math.floor((typeBits * 15) / 63.0);
-	}
+        Type(int colorCode) {
+            this.color = TextColor.fromRgb(colorCode);
+        }
 
-	private int CalcHPDamage(NbtCompound ivs) {
-		int u = (ivs.getInt("hp") % 4) / 2;
-		int v = (ivs.getInt("attack") % 4) / 2;
-		int w = (ivs.getInt("defence") % 4) / 2;
-		int x = (ivs.getInt("speed") % 4) / 2;
-		int y = (ivs.getInt("special_attack") % 4) / 2;
-		int z = (ivs.getInt("special_defence") % 4) / 2;
-
-		int powerBits = u + 2 * v + 4 * w + 8 * x + 16 * y + 32 * z;
-
-		return (int) Math.floor(((powerBits * 40) / 63.0) + 30);
-	}
-
-	private enum Type {
-		FIGHTING(0xC22E28),
-		FLYING(0xA98FF3),
-		POISON(0xA33EA1),
-		GROUND(0xE2BF65),
-		ROCK(0xB6A136),
-		BUG(0xA6B91A),
-		GHOST(0x735797),
-		STEEL(0xB7B7CE),
-		FIRE(0xEE8130),
-		WATER(0x6390F0),
-		GRASS(0x7AC74C),
-		ELECTRIC(0xF7D02C),
-		PSYCHIC(0xF95587),
-		ICE(0x96D9D6),
-		DRAGON(0x6F35FC),
-		DARK(0x705746);
-
-		private final TextColor color;
-
-		Type(int colorCode) {
-			this.color = TextColor.fromRgb(colorCode);
-		}
-
-		public TextColor getColor() {
-			return color;
-		}
-	}
+        public TextColor getColor() {
+            return color;
+        }
+    }
 }
